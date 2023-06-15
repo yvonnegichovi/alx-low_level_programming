95d8cf91233a0bb73e8586ca91237fd12267cd41
yvonnegichovi <yvonnegichovi@gmail.com>
2023-06-15 14:36:54 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  main - A program that prints a line with puts function
+ *  main - entry point
  *  return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
