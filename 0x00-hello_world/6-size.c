8d3ae69fa8eb9f5bef4e375768c291f32588fec9
yvonnegichovi <yvonnegichovi@gmail.com>
2023-06-16 11:18:02 -0700

    Positive anything is better than negative nothing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * funtion - sizeof
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+	long int c;
+	float d;
+	printf("Size of the variable is:%d,%c,%ld,%f bytes", sizeof(a, b, c, d);
+	return (0);
+}
