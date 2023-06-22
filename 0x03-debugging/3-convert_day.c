08ef52573a22810832257e3d2092e86d600fe9cc
yvonnegichovi <yvonnegichovi@gmail.com>
2023-06-22 06:52:13 -0700

    Fix the print_remaining_days() function so that the output works correctly for all dates and all leap years

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -16,34 +16,34 @@ int convert_day(int month, int day)
 			day = 31 + day;
 			break;
 		case 3:
-			day = 59 + day;
+			day = 60 + day;
 			break;
 		case 4:
-			day = 90 + day;
+			day = 91 + day;
 			break;
 		case 5:
-			day = 120 + day;
+			day = 121 + day;
 			break;
 		case 6:
-			day = 151 + day;
+			day = 152 + day;
 			break;
 		case 7:
-			day = 181 + day;
+			day = 182 + day;
 			break;
 		case 8:
-			day = 212 + day;
+			day = 213 + day;
 			break;
 		case 9:
-			day = 243 + day;
+			day = 244 + day;
 			break;
 		case 10:
-			day = 273 + day;
+			day = 274 + day;
 			break;
 		case 11:
-			day = 304 + day;
+			day = 305 + day;
 			break;
 		case 12:
-			day = 334 + day;
+			day = 335 + day;
 			break;
 		default:
 			break;
