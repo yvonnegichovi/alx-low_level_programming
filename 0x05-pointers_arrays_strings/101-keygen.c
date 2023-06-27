241fd95c1d3983d2a47350b49ab130496df62c91
yvonnegichovi <yvonnegichovi@gmail.com>
2023-06-27 12:43:11 -0700

    Create a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,21 +10,21 @@
  */
 int main(void)
 {
-	int pass[150];
+	int pass[100];
 	int i, sum, n;
 
 	sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 150; i++)
+	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((3000 - sum) - '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 3000 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
