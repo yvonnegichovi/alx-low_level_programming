f3641777a23ee01f8fa1878b0293f2cc394e1d84
yvonnegichovi <yvonnegichovi@gmail.com>
2023-07-11 13:33:58 -0700

    malloc and free mandatory and advanced tasks

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i, c;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = c = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[c] != '\0')
+		c++;
+	concat = malloc(sizeof(char) * (i + c + 1));
+
+	if (concat == NULL)
+		return (NULL);
+	i = c = 0;
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+
+	while (s2[c] != '\0')
+	{
+		concat[i] = s2[c];
+		i++, c++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
