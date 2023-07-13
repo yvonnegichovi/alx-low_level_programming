c6711771b979dbbd1e59041c12d300e501fcf546
yvonnegichovi <yvonnegichovi@gmail.com>
2023-07-13 07:32:44 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: input
+ * Return: a ptr if malloc is successful, status value of 98 if otherwise
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
