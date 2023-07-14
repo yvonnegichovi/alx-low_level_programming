81b2ce4a2d2d95bc9f4cf19a10b1be915f6a345b
yvonnegichovi <yvonnegichovi@gmail.com>
2023-07-14 14:46:39 -0700

    more malloc, free advanced tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 #define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
+ *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-
 int is_digit(char *s)
 {
 	int i = 0;
@@ -25,9 +26,9 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
 	int i = 0;
@@ -40,9 +41,8 @@ int _strlen(char *s)
 }
 
 /**
- * error - handles errors for main
+ * errors - handles errors for main
  */
-
 void errors(void)
 {
 	printf("Error\n");
@@ -51,11 +51,11 @@ void errors(void)
 
 /**
  * main - multiplies two positive numbers
- * @argc: number of arguements
- * @argv: array of arguements
- * Return: 0 (Success)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
@@ -68,21 +68,20 @@ int main(int argc, char *argv[])
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 -1; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] += carry;
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
