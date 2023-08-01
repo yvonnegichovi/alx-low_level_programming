a8c02f5cb4e0e394a1f041f65ba4ee4cf604a0b5
yvonnegichovi <yvonnegichovi@gmail.com>
2023-08-01 04:19:01 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 
-	while (k < index && temp)
+	while (k < index && temp != NULL)
 	{
 		temp = temp->next;
 		k++;
