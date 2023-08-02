76f555e46b4a6f502f35696edc44c8b6f7657f00
yvonnegichovi <yvonnegichovi@gmail.com>
2023-08-02 09:28:46 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -72,9 +72,9 @@ size_t print_listint_safe(const listint_t *head)
 		for (index = 0; index < nodes; index++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+		head = head->next;
 		}
-		printf("-> [%p] %d\n", (void *)head, 98);
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (nodes);
 }
