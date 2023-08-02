9bc93e03350442008de688a1746cb5e7929b67be
yvonnegichovi <yvonnegichovi@gmail.com>
2023-08-02 10:00:20 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
