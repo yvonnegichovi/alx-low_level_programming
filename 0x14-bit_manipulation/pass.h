1786ca1edf041819d118e2e2606aebf6e34c03c5
yvonnegichovi <yvonnegichovi@gmial.com>
2023-08-31 22:23:06 -0700

    bitwise task

---
+++ 0x14-bit_manipulation/pass.h
@@ -0,0 +1,29 @@
+
+#ifndef PASS_H
+#define PASS_H
+
+/**
+ * struct char_group - struct
+ * @x: passgroup
+ * @next: next
+ */
+
+typedef struct char_group
+{
+	char_x x;
+	struct char_group *next;
+} char_pass;
+
+/**
+ * struct char_character - struct
+ * @b: character
+ * @next: next
+ */
+
+typedef struct char_character
+{
+	int b;
+	struct char_character *next;
+} char_x;
+
+#endif /* PASS_H */
--- 0x14-bit_manipulation/password_generate.c
+++ 0x14-bit_manipulation/password_generate.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "pass.h"
 
 /**
  * main - Entry point
