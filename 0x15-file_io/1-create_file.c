b5a5705c4d46371b920d29e1b333385027856944
yvonnegichovi <yvonnegichovi@gmail.com>
2023-09-04 12:09:44 -0700

    file i/o tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and if it exists, truncates it
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, _strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
