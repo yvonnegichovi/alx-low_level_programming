20d75d0206ab65b32300000d93c8fdf7f2810fd2
yvonnegichovi <yvonnegichovi@gmail.com>
2023-09-04 12:34:35 -0700

    file i/o tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,9 +12,6 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, bw;
 
-	if (filename == NULL)
-		return (-1);
-
 	fd = open(filename, O_WRONLY |O_APPEND);
 	if (fd == -1)
 		return (-1);
