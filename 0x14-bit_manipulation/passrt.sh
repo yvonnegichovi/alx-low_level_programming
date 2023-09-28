ccf48c492a745d949a499cb81373c109429b416d
yvonnegichovi <yvonnegichovi@gmail.com>
2023-09-28 11:35:12 -0700

    crackme project

--- 0x14-bit_manipulation/passrt.sh
+++ 0x14-bit_manipulation/passrt.sh
@@ -5,4 +5,28 @@ echo -e
 echo -e "Welcome to crackme task..."
 echo -e
 
-curl
+curl https://raw.githubusercontent.com/yvonnegichovi/alx-low_level_programming/master/0x14-bit_manipulation/password_generate.c -o passwordxt.c
+
+sed -i 's/"pass.h"/<stdio.h>/g' passwordxt.c
+sed -i '2i #include <stdlib.h>' passwordxt.c
+
+#Generaiting the password document
+gcc passwordxt.c -o pass.xt
+
+#Generating the password document
+
+./pass.xt
+
+echo -e
+echo -e "Cleanup..."
+
+rm .xt passwordxt.c
+
+#clear
+
+echo -e "Created 101 password successfully"
+ls 101-password
+
+echo -e
+echo -e "Run ./crackme \`cat 101-password\` to verify"
+echo -e
