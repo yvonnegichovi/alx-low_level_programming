bdb4cf71dab6d0b85a91788a8f4fd2a68724c018
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-09 07:48:37 -0800

    double linked list tasks, creating a README.md file, printing the elements of a linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	unsigned int n;
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
