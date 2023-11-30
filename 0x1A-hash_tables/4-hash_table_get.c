d6dece0a5b3490148459b710537feba7c8090c84
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-30 12:18:23 -0800

    considering the case of an empty hash table or key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	current = ht->array[index];
 
