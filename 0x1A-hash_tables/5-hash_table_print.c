66e302b945132d2bc313ed0314f026adaf03d458
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-01 01:43:06 -0800

    handling condition where ht is NULL

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,6 +18,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *current;
 	int printed = 0;
+	
+	if (ht == NULL)
+		return;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
