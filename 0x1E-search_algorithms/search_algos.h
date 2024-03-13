99c67eda590bd2b40230ba3b6c20c79608d6e40f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-13 18:45:49 +0300

    task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,10 @@
 #ifndef SEARCH_ALGOS_H
- #define SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
- int linear_search(int *array, size_t size, int value);
- int binary_search(int *array, size_t size, int value);
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /**SEARCH_ALGOS_H**/
