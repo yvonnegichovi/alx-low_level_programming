4e40b596b68c456ba6becf91698cb077399a1f4e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-14 12:14:12 -0700

    102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,8 +18,18 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0, high = (size - 1);
 	size_t pos;
 
+	if (array == NULL)
+		return (-1);
+
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
+		if  (low == high)
+		{
+			if (array[low] == value)
+			{
+				return (low);
+			}
+		}
 		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
